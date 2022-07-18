2714cf00da0909208012afe8686df9e566c93828
ToThiQuyen <19130187@st.hcmuaf.edu.vn>
2022-07-19 01:53:46 +0700

    Lam giao dien header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,9 +46,10 @@ import { MdbTransferModule } from 'mdb-angular-transfer';
 import { MdbMentionModule } from 'mdb-angular-mention';
 import { MdbCookiesManagementService } from 'mdb-angular-cookies-management';
 import { MdbStorageManagementService } from 'mdb-angular-storage-management';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
