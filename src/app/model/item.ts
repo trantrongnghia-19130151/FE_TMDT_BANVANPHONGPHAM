0018661a5ce4874269ec1d7245f6bf2c2388f7f9
NhuanNHP <19130164@st.hcmuaf.edu.vn>
2022-07-22 10:28:53 +0700

    fix conflict

---
+++ src/app/model/item.ts
@@ -0,0 +1,30 @@
+import {Product} from "./product";
+
+export class Item {
+  private _product:Product;
+  private _quantity:number;
+
+  constructor(product: Product, quantity: number) {
+    this._product = product;
+    this._quantity = quantity;
+  }
+
+  get product(): Product {
+    return this._product;
+  }
+
+  set product(value: Product) {
+    this._product = value;
+  }
+
+  get quantity(): number {
+    return this._quantity;
+  }
+
+  set quantity(value: number) {
+    this._quantity = value;
+  }
+  getTotal(){
+    return this.product.price * this.quantity;
+  }
+}
