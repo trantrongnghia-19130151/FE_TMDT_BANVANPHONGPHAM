6b3225d5c587201a572f4cee1d703bdb770a4b54
ToThiQuyen <19130187@st.hcmuaf.edu.vn>
2022-08-08 02:25:13 +0700

    giao dien trang chu va cai dat json-server

--- src/app/model/category.spec.ts
+++ src/app/model/category.spec.ts
@@ -2,6 +2,7 @@ import { Category } from './category';
 
 describe('Category', () => {
   it('should create an instance', () => {
+    // @ts-ignore
     expect(new Category()).toBeTruthy();
   });
 });
